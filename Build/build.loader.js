7d728e49c85fb9019e3873dbf8ddee8a03c42732
OctoHato <34582182+OctoHato@users.noreply.github.com>
2026-01-06 10:54:33 -0600

    bwa

--- Build/build.loader.js
+++ Build/build.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 97636475,
+    wasmFileSize: 99679358,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
@@ -1411,6 +1411,8 @@ Module.UnityCache = function () {
       reject(msg);
     } else if (!Module.SystemInfo.hasWasm) {
       reject("Your browser does not support WebAssembly.");
+    } else if (!Module.SystemInfo.hasWasm2023) {
+      reject("Your browser does not support WebAssembly 2023. Please update to Chrome ≥ 91 (May 2021), Firefox ≥ 89 (June 2021) or Safari ≥ 16.4 (March 2023). (failed " + Module.SystemInfo.missingWasm2023Feature + ")");
     } else {
       Module.startupErrorHandler = reject;
       onProgress(0);
